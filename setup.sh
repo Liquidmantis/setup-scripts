9cc4aef2c0059e8ae38f49706b9b9981d87ef2a1
Liquidmantis <liquidmantis@gmail.com>
2022-05-17 10:54:21 -0600

    fix: issues from realworld run

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ if [[ $? != 0 ]] ; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+echo "Adding Homebrew to environment..."
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 if [[ $(uname) == "Linux" ]]; then
   test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
   test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
@@ -28,5 +31,13 @@ fi
 echo "Installing/updating Ansible community.general modules..."
 ansible-galaxy collection verify community.general
 
+if ![[ -f "./playbook.yml" ]] ; then
+  echo "Downloading Ansible playbook..."
+  playbook_path="/tmp/lm-setup-playbook.yml"
+  curl -s https://raw.githubusercontent.com/Liquidmantis/setup-scripts/main/playbook.yml -o $(playbook_path)
+else
+  playbook_path="./playbook.yml"
+fi
+ 
 echo "Executing Ansible playbook"
-ansible-playbook playbook.yml
+ansible-playbook $playbook_path
